c9979ff0ef4f16902b91fbc3e75562670d966aef
yigityanik <yeyanik123@gmail.com>
2026-05-10 19:57:08 +0300

    shamrock: rootdir: add init.zram.rc in Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -38,6 +38,13 @@ LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH	:=  $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.zram.rc
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.zram.rc
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
 # Configuration scripts
 
 include $(CLEAR_VARS)

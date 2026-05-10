d7ad8bb244366c0bd83e11e4ac0acc4f9d20bb67
yigityanik <yeyanik123@gmail.com>
2026-05-10 19:17:53 +0300

    shamrock: build thermal@1.0-service instead of mock thermal

--- device.mk
+++ device.mk
@@ -281,8 +281,9 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.mock \
-    thermal.msm8952
+    thermal.msm8952 \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf

0182bd5b57c3d339b1d3749d23c2c8e2e9df122b
yigityanik <yeyanik123@gmail.com>
2026-05-10 19:44:41 +0300

    Initialize Android One Experience

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_shamrock.mk
+    $(LOCAL_DIR)/aosp_shamrock.mk
--- aosp_shamrock.mk
+++ aosp_shamrock.mk
@@ -19,16 +19,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common aosp stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from shamrock device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+## Android One Experience required flags
+# if your build is ready to release:
+#CUSTOM_BUILD_TYPE := release
+PRODUCT_CUSTOM_MODEL := GM5P
+TARGET_GAPPS_ARCH=arm64
+
+# Gallery2
+PRODUCT_PACKAGES += \
+    Gallery2
+
+## Android One Experience Required Flags end
+
 PRODUCT_BRAND := GM
 PRODUCT_DEVICE := shamrock
 PRODUCT_MANUFACTURER := General Mobile
-PRODUCT_NAME := lineage_shamrock
+PRODUCT_NAME := aosp_shamrock
 PRODUCT_MODEL := GM 5 Plus
 
 PRODUCT_GMS_CLIENTID_BASE := android-gm

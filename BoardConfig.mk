fcbfdaf9bb748f0a92ca150a959284e9a8185d36
yigityanik <yeyanik123@gmail.com>
2026-06-07 16:37:15 +0300

    shamrock: use msm-perf_defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,7 +157,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/GM/shamrock
-TARGET_KERNEL_CONFIG := test_defconfig
+TARGET_KERNEL_CONFIG := msm-perf_defconfig
 TARGET_KERNEL_VERSION := 3.10
 
 # Security patch level
